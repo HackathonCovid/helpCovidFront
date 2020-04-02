1cc8c89159e46d69f3ea507d638ffcd623085572
luffy <kosseyla.hamouche@gmail.com>
2020-04-02 18:38:21 +0200

    add login and register

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+
+import history from '../../history';
+import {entrypoint} from "../../entrypoint";
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+
+export default function SignInSide() {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [error, setError] = React.useState(false);
+    const classes = useStyles();
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setError(false);
+    };
+
+    function login(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        fetch(`${entrypoint}/api/login`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if(data.hasOwnProperty('error')) {
+                    setError(true);
+                } else if (data.response.hasOwnProperty('id')) {
+                    setError(false);
+                    const user = data.response;
+                    localStorage.setItem('user', JSON.stringify(user));
+                    history.push('/');
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            { error && <Snackbar open={error} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error">
+                    Email et Password,
+                    Merci de vérifier vos identfiants.
+                </Alert>
+            </Snackbar> }
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Se connecter
+                </Typography>
+                <form className={classes.form} onSubmit={login}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        autoComplete="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        autoFocus
+                    />
+
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Mot de passe"
+                        type="password"
+                        id="password"
+                        autoComplete="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Se connecter
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Mot de passe oublié ?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/home" variant="body2">
+                                {"S'inscrire ?"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+            </Box>
+        </Container>
+
+    );
+}
